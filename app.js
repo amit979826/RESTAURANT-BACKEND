4643853e03f819bc4e2577a4ce501b3c75256aee
amit979826 <aamitsingh821@gmail.com>
2026-01-19 16:44:19 +0530

    Fix CORS for Vercel frontend

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import {dbConnection} from "./database/dbConnection.js"
+// import {dbConnection} from "./database/dbConnection.js"
 import {errorMiddleware} from "./error/error.js";
 import reservationRouter from "./routes/reservationRoute.js"
 
@@ -9,18 +9,26 @@ const app = express()
 dotenv.config({path:"./config/config.env"}) 
 
 app.use(
-    cors({
-        origin:[process.env.FRONTEND_URL],
-        methods:["POST"],
-        credentials:true,
-    })
+  cors({
+    origin: [
+      "https://restaurant-frontend-five-xi.vercel.app",
+      "http://localhost:5173"
+    ],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
 );
+
+
+app.get("/", (req, res) => {
+  res.send("Restaurant Backend API is running 🚀");
+});
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use("/api/v1/reservation",reservationRouter);   
 
-dbConnection();
+// dbConnection();
 
 app.use(errorMiddleware);
 
--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 import app from './app.js';
+import { dbConnection } from './database/dbConnection.js';
 
-app.listen(process.env.PORT,()=>{
+dbConnection();
+
+if(process.env.VERCEL !== "1"){
+  const PORT = process.env.PORT || 4000;
+
+  app.listen(process.env.PORT,()=>{
   console.log(`server Running On Port ${process.env.PORT}`);  
 })
+}
+
