425c4e4683ba77537f88510de6d5e94991e628d1
amit979826 <aamitsingh821@gmail.com>
2026-01-20 14:33:06 +0530

    modifies server

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ dotenv.config({path:"./config/config.env"})
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL,
-      "http://localhost:5173"
+      process.env.FRONTEND_URL
     ],
     credentials: true,
   })
@@ -37,3 +36,4 @@ app.use("/api/v1/reservation",reservationRouter);
 app.use(errorMiddleware);
 
 export default app
+
--- server.js
+++ server.js
@@ -1,13 +1,10 @@
-import app from './app.js';
-import { dbConnection } from './database/dbConnection.js';
+import app from "./app.js";
+import { dbConnection } from "./database/dbConnection.js";
 
 dbConnection();
 
-if(process.env.VERCEL !== "1"){
-  const PORT = process.env.PORT || 4000;
-
-  app.listen(process.env.PORT,()=>{
-  console.log(`server Running On Port ${process.env.PORT}`);  
-})
-}
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
